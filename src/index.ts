71ef2c766fc39129d1b8ebbb5b8ab92467675bdc
Griffin Gradke <griffin.gradke@gmail.com>
2023-05-11 16:50:02 -0500

    Change Sorter to Abstract class (fixed numbers)

--- src/index.ts
+++ src/index.ts
@@ -5,32 +5,37 @@ import { LinkedList } from "./LinkedList";
 
 console.log("------Start-----");
 
-//Init values from classes
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const string = new CharactersCollection("DfwgESGS");
-const linkedList = new LinkedList();
-linkedList.add(500);
-linkedList.add(-10);
-linkedList.add(-3);
-linkedList.add(24);
+const numberCollection = new NumbersCollection([10, 3, -5, 0]);
+console.log(numberCollection.data);
+numberCollection.sort();
+console.log(`Sorted: ${numberCollection.data}`);
 
-//Init Sorters
-const sorterArr = new Sorter(numbersCollection);
-const sorterStr = new Sorter(string);
-const sorterLL = new Sorter(linkedList);
+// //Init values from classes
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const string = new CharactersCollection("DfwgESGS");
+// const linkedList = new LinkedList();
+// linkedList.add(500);
+// linkedList.add(-10);
+// linkedList.add(-3);
+// linkedList.add(24);
 
-//Log initial values
-console.log(numbersCollection.data);
-console.log(string.data);
-linkedList.print();
+// //Init Sorters
+// const sorterArr = new Sorter(numbersCollection);
+// const sorterStr = new Sorter(string);
+// const sorterLL = new Sorter(linkedList);
 
-//Call for sorting
-sorterArr.sort();
-sorterStr.sort();
-sorterLL.sort();
+// //Log initial values
+// console.log(numbersCollection.data);
+// console.log(string.data);
+// linkedList.print();
 
-//Log Sorted Values
-console.log(`Sorted Array: ${numbersCollection.data}`);
-console.log(`Sorted String: ${string.data}`);
-console.log(`Sorted LL:`);
-console.log(linkedList.print());
+// //Call for sorting
+// sorterArr.sort();
+// sorterStr.sort();
+// sorterLL.sort();
+
+// //Log Sorted Values
+// console.log(`Sorted Array: ${numbersCollection.data}`);
+// console.log(`Sorted String: ${string.data}`);
+// console.log(`Sorted LL:`);
+// console.log(linkedList.print());
