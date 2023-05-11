2a8992c56bdada9734a0df39de1323545aad37d6
Griffin Gradke <griffin.gradke@gmail.com>
2023-05-11 16:56:21 -0500

    abstract support for strings and Linked lists

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,37 @@
-import { Sorter } from "./Sorter";
+//import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
 console.log("------Start-----");
 
+console.log("Numbers Array");
 const numberCollection = new NumbersCollection([10, 3, -5, 0]);
 console.log(numberCollection.data);
 numberCollection.sort();
 console.log(`Sorted: ${numberCollection.data}`);
+console.log("-----------------");
+
+console.log("Characters");
+const charactersCollection = new CharactersCollection("DfwgESGS");
+console.log(charactersCollection.data);
+charactersCollection.sort();
+console.log(`Sorted: ${charactersCollection.data}`);
+console.log("-----------------");
+
+console.log("Linked List");
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(24);
+
+linkedList.print();
+linkedList.sort();
+
+console.log(`Sorted:`);
+linkedList.print();
+console.log("-----------------");
 
 // //Init values from classes
 // const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
