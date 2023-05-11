5bfb57a85fea184cb716d0bef66c598f7673fae1
Griffin Gradke <griffin.gradke@gmail.com>
2023-05-11 15:22:38 -0500

    added string support

--- build/index.js
+++ build/index.js
@@ -2,9 +2,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
 console.log("------Start-----");
+//Init values from classes
 const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter_1.Sorter(numbersCollection);
+const string = new CharactersCollection_1.CharactersCollection("DfwgESGS");
+//Init Sorters
+const sorterArr = new Sorter_1.Sorter(numbersCollection);
+const sorterStr = new Sorter_1.Sorter(string);
+//Log initial values
 console.log(numbersCollection.data);
-sorter.sort();
-console.log(`Sorted: ${numbersCollection.data}`);
+console.log(string.data);
+//Call for sorting
+sorterArr.sort();
+sorterStr.sort();
+//Log Sorted Values
+console.log(`Sorted Array: ${numbersCollection.data}`);
+console.log(`Sorted String: ${string.data}`);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 console.log("------Start-----");
+
+//Init values from classes
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+const string = new CharactersCollection("DfwgESGS");
+
+//Init Sorters
+const sorterArr = new Sorter(numbersCollection);
+const sorterStr = new Sorter(string);
+
+//Log initial values
 console.log(numbersCollection.data);
-sorter.sort();
-console.log(`Sorted: ${numbersCollection.data}`);
+console.log(string.data);
+
+//Call for sorting
+sorterArr.sort();
+sorterStr.sort();
+
+//Log Sorted Values
+console.log(`Sorted Array: ${numbersCollection.data}`);
+console.log(`Sorted String: ${string.data}`);
